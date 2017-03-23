7a0053f969b132f5e9c074fdd6cfb47c7b6a37d9
Alexzarazua <alexzarazua99@gmail.com>
2017-03-23 20:40:31 +0100

    first commit

--- pruebalinea.sh
+++ pruebalinea.sh
@@ -1,2 +1,17 @@
+#!/bin/bash
+#categori aleatoriamente
+clear
+
+
 lineas=`ls -l  | wc | tr -s " " | cut -d" " -f2`
+aleatorio=$lineas
 let=$lineas-1
+
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo " `cat $lineas  "
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+
+
+#tambien habia pensado esto aleatorio=`ls -l  | wc | tr -s " " | cut -d" " -f2`
+#o aleatorio=$lineas`ls -l  | wc | tr -s " " | cut -d" " -f2`
+#pero no estoy muy seguro
