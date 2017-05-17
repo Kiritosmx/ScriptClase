e7ee0c1ca17a87159b48372c526e2b0c8c81ad6d
Alexzarazua <alexzarazua99@gmial.com>
2017-05-17 12:46:59 +0200

    funcion duracion falata acabarla

--- muestrapreguntas.sh
+++ muestrapreguntas.sh
@@ -46,26 +46,39 @@ function pregunta {
 }
 
 
-function puntos {
+function duracion {
+
   clear
+  inicio=`date | cut -d" " -f4`
+  read
+  fin=`date | cut -d" " -f4`
+
+  hinicio=`echo $inicio | cut -d: -f1`
+  minicio=`echo $inicio | cut -d: -f2`
+  sinicio=`echo $inicio | cut -d: -f3`
+
+  let hinicio=$hinicio*3600
+  let minicio=$minicio*60
+  let sinicio=$sinicio+$minicio+$hinicio
+  #Segundos del primer valor
+
+
+  hfin=`echo $fin | cut -d: -f1`
+  mfin=`echo $fin | cut -d: -f2`
+  sfin=`echo $fin | cut -d: -f3`
+
+  let hfin=$hfin*3600
+  let mfin=$mfin*60
+  let sfin=$sfin+$mfin+$hfin
+  #Segundos del segundo valor
+  let segtrans=$sfin-$sinicio
+  echo "Han pasado $segtrans''"
 
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-#HAY QUE IR ACABANDO LA FUNCION DEL RESULTADO SEGÚN LAS PREGUNTAS ACERTADAS Y EL TIEMPO
+#HAY QUE IR ACABANDO LA FUNCION DEL RESULTADO SEGÚN LAS PREGUNTAS ACERTADAS Y EL TIEMPO QUE HAS TARDADO EN RESPONDER A ELLAS
 
 function final {
   clear
@@ -75,7 +88,7 @@ function final {
 
 }
 
-
+tiempotardado=$duracion
 
 echo "$1" > /tmp/categoria.txt
 categoria=`cat /tmp/categoria.txt`
